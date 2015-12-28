f9b05ef685aa6662cb35b0d2dfcd84788ef6afce
rov <valadares.thel@gmail.com>
2015-12-28 10:40:00 -0200

    first commit

---
+++ FoodTrack/RatingControl.swift
@@ -0,0 +1,33 @@
+//
+//  RatingControl.swift
+//  FoodTrack
+//
+//  Created by Raoni Valadares on 12/28/15.
+//  Copyright © 2015 None. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIView {
+
+  // MARK: Initialization
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 44, height: 44))
+        button.backgroundColor = UIColor.redColor()
+        
+        button.addTarget(self, action: "ratingButtonTapped:", forControlEvents: .TouchDown)
+        
+        addSubview(button)
+    }
+    
+    override func intrinsicContentSize() -> CGSize {
+        return CGSize(width: 240, height: 44)
+    }
+    
+    // MARK: Button Action
+    func ratingButtonTapped(button: UIButton) {
+        print("Button pressed 👍")
+    }
+}
