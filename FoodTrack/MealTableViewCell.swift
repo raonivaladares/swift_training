97f93f45a1c205e824bd3f6e34f0d0937444ee94
Raoni de Oliveira Valadares <valadares.thel@gmail.com>
2016-02-01 20:21:18 -0200

    Adding images and outlets to MealTabViewCell

---
+++ FoodTrack/MealTableViewCell.swift
@@ -0,0 +1,11 @@
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+    
+    // MARK: Properties
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
+    
+}
